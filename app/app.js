9d46dbd2cd7098335828ac1ae2ea6289eb63ba08
Nikolai <kuschaev@users.noreply.github.com>
2017-10-17 05:15:33 +0300

    so commit very diff much message

--- app/app.js
+++ app/app.js
@@ -8,10 +8,21 @@ angular.module('myApp', [
   'ngSanitize',
   'myApp.view1',
   'myApp.view2',
+  'myApp.view3',
   'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+])
+.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/view2'});
+}])
+.controller('AppCtrl', ['$scope', function($scope) {
+
+    $scope.currentNavItem = 'view2';
+    /*
+    $scope.goto = function(page) {
+      console.log("Goto" + page);
+    };
+    */
 }]);
+
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,14 +1,61 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+angular.module('myApp.view2', ['ngRoute', 'ngMaterial', 'ngMessages', 'ngSanitize', 'ngVis'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
-  });
+    $routeProvider.when('/view2', {
+        templateUrl: 'view2/view2.html',
+        controller: 'View2Ctrl'
+    });
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('View2Ctrl', ['$scope', 'VisDataSet',
+    function($scope, VisDataSet) {
 
-}]);
+        var dataGroups = new VisDataSet();
+        dataGroups.add({});
+
+
+        var dataItems = new VisDataSet();
+        dataItems.add([
+            {x: '2017-07-10', y: 40},
+            {x: '2017-08-01', y: 30},
+            {x: '2017-08-09', y: 60},
+            {x: '2017-08-11', y: 5},
+            {x: '2017-08-16', y: 45},
+            {x: '2017-08-23', y: 35},
+            {x: '2017-08-28', y: 70},
+            {x: '2017-08-31', y: 0},
+            {x: '2017-09-01', y: 1},
+            {x: '2017-09-02', y: 0},
+            {x: '2017-09-06', y: 2},
+            {x: '2017-09-16', y: 15},
+            {x: '2017-09-18', y: 90},
+            {x: '2017-10-02', y: 10},
+            {x: '2017-10-10', y: 55},
+            {x: '2017-10-17', y: 40},
+         ]);
+
+        $scope.data = {
+            items: dataItems,
+            groups: dataGroups
+        };
+
+        $scope.events = {
+            timechanged: function() {
+                // ???
+            },
+            rangechanged: function() {
+                // EXPLAIN DansGame
+            }
+        };
+
+        $scope.options = {
+            start: '2017-07-10',
+            end: '2017-10-21',
+            autoResize: true,
+            width: '100%',
+        };
+
+    }
+]);
